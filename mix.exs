a8af82da025ad298d57102ece10b26955b7266df
Powell Kinney <628952+pkinney@users.noreply.github.com>
2023-01-18 15:56:56 -0600

    Change minimum elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Replay.MixProject do
     [
       app: :replay,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
